ebe9662ed8802c7da68ad09040f0ffdd0349426d
veronar <vesingh@student.wethinkcode.co.za>
2019-10-09 11:53:15 +0200

    ex01 done

--- d02/ex01/one_more_time.php
+++ d02/ex01/one_more_time.php
@@ -19,39 +19,37 @@ $month = array(
 	8 => '/[Ss]eptembre/',
 	9 => '/[Oo]ctobre/',
 	10 => '/[Nn]ovembre/',
-	11 => '/[Dd][ée]cembre/');
-
-
-$week = array(
-	1 => "lundi",
-	2 => "mardi",
-	3 => "mercredi",
-	4 => "jeudi",
-	5 => "vendredi",
-	6 => "samedi",
-	7 => "dimanche");
+	11 => '/[Dd][ée]cembre/'); 
+	// square brackets is for options https://medium.com/factory-mind/regex-tutorial-a-simple-cheatsheet-by-examples-649dc1c3f285
 
 $replace = array( "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12");
 
-// if ($argc < 2) // checks for arguments
-// 	return;
+if ($argc < 2) // checks for arguments
+	return;
 
-// if (!preg_match('/^([Ll]undi||[Mm]ardi|[Mm]ercredi|[Jj]eudi|[Vv]endredi|[Ss]amedi|[Dd]imanche) [0-9][0-9]? ([Jj]anvier|[Ff][eé]vrier|[Mm]ars|[Aa]vril|[Mm]ai|[Jj]uin|[Jj]uillet|[Aa]o[uû]t|[Ss]eptembre|[Oo]ctobre|[Nn]ovembre|[Dd][ée]cembre) [0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}$/', $argv[1])) { //checks for valid input
-// 	echo "Wrong format\n";
-// 	return;
-// }
+if (!preg_match('/^([Ll]undi||[Mm]ardi|[Mm]ercredi|[Jj]eudi|[Vv]endredi|[Ss]amedi|[Dd]imanche) [0-9][0-9]? ([Jj]anvier|[Ff][eé]vrier|[Mm]ars|[Aa]vril|[Mm]ai|[Jj]uin|[Jj]uillet|[Aa]o[uû]t|[Ss]eptembre|[Oo]ctobre|[Nn]ovembre|[Dd][ée]cembre) [0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}$/', $argv[1])) { //checks for valid input ie that there is a dayof week & month with either upper or lower case, date in numerical & valid time in numerical.  
+	echo "Wrong format\n";
+	return;
+}
 
-unset($argv[0]); // to remove program name ***.php from array
+unset($argv[0]); // to remove program name ***.php (name of file) from array
 
 $arr = ft_split($argv[1]); // split 1st argument into an array
 
-// if (count($arr) != 5){ // check format of arguments recieved
-// 	echo "Wrong format\n";
-// 	return;
-// }
+if (count($arr) != 5){ // another check: format of arguments recieved ie that there are 5
+	echo "Wrong format\n";
+	return;
+}
 
-$arr[2] = preg_replace($month, $replace, $arr[2]);
+$arr[2] = preg_replace($month, $replace, $arr[2]); // replaces the name the month with the numerical value of the month
 
-print_r($arr);
+$seconds = strtotime("$arr[3]-$arr[2]-$arr[1] $tab[4]"); //https://www.php.net/manual/en/function.strtotime.php
+
+if (!$seconds){
+	echo "Wrong format\n";
+	return;
+}
+
+echo "$seconds\n";
 
 ?>
