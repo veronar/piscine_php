c0d2e009362e997b4a0c3eec55e0779a26a93dc9
veronar <vesingh@student.wethinkcode.co.za>
2019-10-14 19:48:28 +0200

    ex01

---
+++ d04/ex01/create.php
@@ -0,0 +1,37 @@
+<?php
+	session_start();
+
+	function read_data()
+	{
+		if (issset($_POST['login']) && $_POST['login'] != NULL && 
+		isset($_POST['passwd']) && $_POST['passwd'] != NULL &&
+		$_POST['submit'] && $_POST['submit'] === "OK")
+		{
+			$arr['login'] = $_POST['login'];
+			$arr['passwd'] = hash('whirlpool', $_POST['passwd']);
+		}
+		else {
+			echo "Error\n";
+			exit();
+		}
+	}
+
+	$arr = read_data();
+	if (!file_exists("../private") || !file_exists("../private/passwd"))
+		mkdir("../private");
+	if (file_exists("../private/passwd"))
+	{
+		$arr_store = unserialize(file_get_contents("../private/passwd"));
+		foreach ($arr_store as $user)
+		{
+			if ($user["login"] === $_POST["login"])
+			{
+				echo "ERROR\n";
+				return ;
+			}
+		}
+	}
+	$ar_store[] = $arr;
+	file_put_contents("../private/passwd", serialize($arr_store));
+	echo "OK\n";
+?>
