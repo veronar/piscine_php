0f27bf9c92e27d98660ecb3343b87b8d24d6dc67
veronar <vesingh@student.wethinkcode.co.za>
2019-10-18 17:06:59 +0200

    ex03

---
+++ d07/ex03/House.class.php
@@ -0,0 +1,16 @@
+<?php
+	 abstract class House {
+	 	abstract function getHouseName();
+	 	abstract function getHouseMotto();
+	 	abstract function getHouseSeat();
+		 
+		 function introduce() {
+	 		print("House ");
+	 		print($this->getHouseName());
+	 		print(" of ");
+	 		print($this->getHouseSeat());
+	 		print(" : ");
+	 		print('"' . $this->getHouseMotto() . '"'  . "\n");
+	 	}
+	 } 
+?>
