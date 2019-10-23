34cefe27a9520c6291962541ff92a16888b58cf6
veronar <vesingh@student.wethinkcode.co.za>
2019-10-23 18:42:58 +0200

    ex02 progress

--- d09/ex02/todo.js
+++ d09/ex02/todo.js
@@ -1,4 +1,6 @@
-
+var tab = [];
+var index = 0;
+var flag = 0;
 
 function addnew(elem) {
 	
@@ -18,6 +20,37 @@ function addnew(elem) {
 		newitem.setAttribute("class", "elems");
 		newitem.setAttribute("onclick", "del(this)");
 		newitem.setAttribute("index", index);
-	}
 
+		var textnode = document.createTextNode(todo);
+		tab[index] = todo;
+		index++;
+		newitem.appendChild(textnode);
+		list.insertBefore(newitem, list.childNodes[0]);
+	}
+}
+
+function del(elem) {
+	if (confirm("Are you sure you would like to delete this entry?") == true) {
+		var i = elem.getAttribute("index");
+		tab.splice(i, 1);
+		elem.parentNode.removeChild(elem);
+		update_cookies();
+	}
+}
+
+function update_cookies() {
+	var newcookie = JSON.stringify(tab);
+	document.cookie = "todos="+newcookie;
+}
+
+window.onload = function() {
+	if (document.cookie) {
+		flag = 1;
+		var iscookie = document.cookie;
+		var newtab = iscookie.split("=");
+		var test = JSON.parse(newtab[1]);
+		for (elem in test)
+			add_new(test[elem]);
+		flag = 0;
+	}
 }
