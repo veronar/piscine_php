69537e7b01307a3d2d9d26f6ca5408dd34071e0a
veronar <vesingh@student.wethinkcode.co.za>
2019-10-24 18:46:20 +0200

    ex03 done

---
+++ d09/ex02bis/todo.js
@@ -0,0 +1,40 @@
+var iscookie = [];
+var ft_list;
+
+$(document).ready(function(){
+	$('#new').click(addnew);
+	$('#ft_list div').click(del);
+	ft_list = $('#ft_list');
+	var temp = document.cookie;
+	if (temp && temp != '') {
+		iscookie = JSON.parse(temp);
+		iscookie.forEach(function (e) {
+			addTodo(e);
+		})
+	}
+});
+
+$(window).on('beforeunload', function(){
+	var todo = ft_list.children();
+	var newcookie = [];
+	for (var i = 0; i < todo.length; i++)
+		newcookie.unshift(todo[i].innerHTML);
+	document.cookie = JSON.stringify(newcookie);
+});
+
+function addnew() {
+	var todo = prompt("What would you like to add?");
+	if (todo) {
+		addTodo(todo);
+	}
+}
+
+function addTodo(todo){
+	ft_list.prepend($('<div class="elems">' + todo + '</div>').click(del));
+}
+
+function del(elem) {
+	if (confirm("Are you sure you would like to delete this entry?") == true) {
+		this.remove();
+	}
+}
